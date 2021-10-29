58319f6f0a822e0980151d3f9ddde1f300400d81
pmsgng <89690526+pmsgng@users.noreply.github.com>
2021-10-29 23:12:28 +0300

    Create game.js

---
+++ game.js
@@ -0,0 +1,43 @@
+for (let i=0; i<9; i++) (
+    document.getElementById('game').innerHTML+='<div class="block"></div>'
+
+)
+let hod = 0;
+document.getElementById('game').onclick = function(event) {
+    console.log(event);
+    if (event.target.className == 'block') {
+        if(hod %2 == 0) {
+            event.target.innerHTML = 'x';
+        }else {
+            event.target.innerHTML = 'o';
+        }
+        hod++;
+        checkWinner()
+    }
+}
+
+function checkWinner() {
+    let allblock = document.getElementsByClassName('block')
+    //console.log(allblock);
+    if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') alert('Крестики победили')
+    if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') alert('Крестики победили')
+    if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
+    if (allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Крестики победили')
+    if (allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') alert('Крестики победили')
+    if (allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
+    if (allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
+    if (allblock[2].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Крестики победили')
+    //нолики
+    if (allblock[0].innerHTML=='o' && allblock[1].innerHTML=='o' && allblock[2].innerHTML=='o') alert('Победили нолики')
+    if (allblock[3].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[5].innerHTML=='o') alert('Победили нолики')
+    if (allblock[6].innerHTML=='o' && allblock[7].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
+    if (allblock[0].innerHTML=='o' && allblock[3].innerHTML=='o' && allblock[6].innerHTML=='o') alert('Победили нолики')
+    if (allblock[1].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[7].innerHTML=='o') alert('Победили нолики')
+    if (allblock[2].innerHTML=='o' && allblock[5].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
+    if (allblock[0].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
+    if (allblock[2].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[6].innerHTML=='o') alert('Победили нолики')
+}
+
+//012 
+//345
+//678
