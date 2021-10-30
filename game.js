b2b265380de97c19f5dc341d17457414777dfa33
V T <pmsgng88@gmail.com>
2021-10-30 14:47:12 +0300

    игра крестики-нолики

--- game.js
+++ game.js
@@ -19,25 +19,168 @@ document.getElementById('game').onclick = function(event) {
 function checkWinner() {
     let allblock = document.getElementsByClassName('block')
     //console.log(allblock);
-    if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') alert('Крестики победили')
-    if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') alert('Крестики победили')
-    if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
-    if (allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Крестики победили')
-    if (allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') alert('Крестики победили')
-    if (allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
-    if (allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') alert('Крестики победили')
-    if (allblock[2].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x') alert('Крестики победили')
-    //нолики
-    if (allblock[0].innerHTML=='o' && allblock[1].innerHTML=='o' && allblock[2].innerHTML=='o') alert('Победили нолики')
-    if (allblock[3].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[5].innerHTML=='o') alert('Победили нолики')
-    if (allblock[6].innerHTML=='o' && allblock[7].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
-    if (allblock[0].innerHTML=='o' && allblock[3].innerHTML=='o' && allblock[6].innerHTML=='o') alert('Победили нолики')
-    if (allblock[1].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[7].innerHTML=='o') alert('Победили нолики')
-    if (allblock[2].innerHTML=='o' && allblock[5].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
-    if (allblock[0].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[8].innerHTML=='o') alert('Победили нолики')
-    if (allblock[2].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[6].innerHTML=='o') alert('Победили нолики')
+    if (allblock[0].innerHTML=='x' && allblock[1].innerHTML=='x' && allblock[2].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      }  
+    
+    if (allblock[3].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[5].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    
+    if (allblock[6].innerHTML=='x' && allblock[7].innerHTML=='x' && allblock[8].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    if (allblock[0].innerHTML=='x' && allblock[3].innerHTML=='x' && allblock[6].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      }  
+    if (allblock[1].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[7].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    if (allblock[2].innerHTML=='x' && allblock[5].innerHTML=='x' && allblock[8].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      }  
+    if (allblock[0].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[8].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    if (allblock[2].innerHTML=='x' && allblock[4].innerHTML=='x' && allblock[6].innerHTML=='x') {
+        alert('Крестики победили')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    //----------------------нолики
+    if (allblock[0].innerHTML=='o' && allblock[1].innerHTML=='o' && allblock[2].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+      } 
+    if (allblock[3].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[5].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[6].innerHTML=='o' && allblock[7].innerHTML=='o' && allblock[8].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[0].innerHTML=='o' && allblock[3].innerHTML=='o' && allblock[6].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[1].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[7].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[2].innerHTML=='o' && allblock[5].innerHTML=='o' && allblock[8].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[0].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[8].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
+    if (allblock[2].innerHTML=='o' && allblock[4].innerHTML=='o' && allblock[6].innerHTML=='o') {
+        alert('Победили нолики')
+        function showModal(id) {
+            return document.getElementById(id).style.display = 'flex';
+        }
+        showModal('modal');
+    }
+    document.getElementById('button').onclick = function() {
+        return document.getElementById('modal').style.display = 'none' && window.location.reload();
+    }
 }
 
-//012 
-//345
-//678
